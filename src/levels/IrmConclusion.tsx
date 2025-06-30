7cf13a1d8a2e4093022abae419de7501ed7d9539
AhsabShiraz1 <shiraz@fabrik.space>
2025-06-30 12:44:24 +0530

    removed the home button from level 4

--- src/levels/IrmConclusion.tsx
+++ src/levels/IrmConclusion.tsx
@@ -91,7 +91,7 @@ export default function IrmConclusion({ setNotification, setCurrentLevel }: Leve
           >
             <Container flexDirection="column" alignItems="center" justifyContent="center">
               <Text fontSize={12} fontWeight="bold">
-                Enter your session ID to proceed:
+                Enter your employee ID to proceed:
               </Text>
               <Text fontSize={12} fontWeight="bold">
                 Skipping this wont register your score
@@ -99,7 +99,7 @@ export default function IrmConclusion({ setNotification, setCurrentLevel }: Leve
               <Input
                 type="text"
                 value={inputValue}
-                placeholder="Enter Session ID"
+                placeholder="Enter employee ID"
                 onValueChange={(value) => setInputValue(value)}
                 marginY={10}
                 borderColor="white"
